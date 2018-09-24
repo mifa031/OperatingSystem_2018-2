a8467bc2d5f2b393cfc7c66c51b1b41780053808
mifa031 <scifimov@naver.com>
2018-09-24 19:38:17 +0900

    Add files via upload

--- 01.Kernel32/VirtualOS.asm
+++ 01.Kernel32/VirtualOS.asm
@@ -5,34 +5,21 @@ SECTION .text
 
 jmp 0x1000:START
 
-SECTORCOUNT dw 0x0000
-TOTALSECTORCOUNT equ 1024
+SECTORCOUNT: dw 0x0000
 
 START:
     mov ax, cs
     mov ds, ax
     mov ax, 0xB800
-
     mov es, ax
 
-    %assign i  0
-    %rep TOTALSECTORCOUNT
-        %assign i  i+1
+    mov ax, 2
+    mul word[SECTORCOUNT]
+    mov si, ax
+    mov byte[es:si+(160*6)], '0'
 
-        mov ax, 2
+    add word[SECTORCOUNT], 1
 
-        mul word[SECTORCOUNT]
-        mov si, ax
+    jmp $
 
-        mov byte[es:si + (160 * 3)], '0' + (i % 10)
-        add word[SECTORCOUNT], 1
-
-        %if i == TOTALSECTORCOUNT
-            jmp $
-        %else
-            jmp (0x1000 + i * 0x20): 0x0000
-        %endif
-
-        times (512 - ($ - $$) % 512) db 0x00
-
-    %endrep
+    times 512 - ($ - $$) db 0x00
